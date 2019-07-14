a8f991e9590f4325a5a4ff16c6cfebade859273a
Michael Meng <michael_meng@Michaels-MacBook-Pro.local>
2019-07-14 17:53:22 -0400

    added mongo uri to source code

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 function db() {
-  const db = config.get("db");
+  //const db = config.get("db");
+  const db =
+    "mongodb://vidlyuser:1234@vidly-shard-00-00-fmuhp.mongodb.net:27017,vidly-shard-00-01-fmuhp.mongodb.net:27017,vidly-shard-00-02-fmuhp.mongodb.net:27017/test?ssl=true&replicaSet=vidly-shard-0&authSource=admin&retryWrites=true&w=majority";
   mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 }
 
