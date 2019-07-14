b8ab67eeca5e439c600d06c225fca78b4d3351fe
Michael Meng <michael_meng@Michaels-MacBook-Pro.local>
2019-07-14 17:57:10 -0400

    removed local uri

--- startup/db.js
+++ startup/db.js
@@ -3,9 +3,7 @@ const mongoose = require("mongoose");
 const config = require("config");
 
 function db() {
-  //const db = config.get("db");
-  const db =
-    "mongodb://vidlyuser:1234@vidly-shard-00-00-fmuhp.mongodb.net:27017,vidly-shard-00-01-fmuhp.mongodb.net:27017,vidly-shard-00-02-fmuhp.mongodb.net:27017/test?ssl=true&replicaSet=vidly-shard-0&authSource=admin&retryWrites=true&w=majority";
+  const db = config.get("db");
   mongoose.connect(db).then(() => winston.info(`Connected to ${db}...`));
 }
 
