8c49e63f7ea500bac915ce35b57bc24df257aaec
Michael Meng <michael_meng@Michaels-MacBook-Pro.local>
2019-07-15 22:02:37 -0400

    modify user route file

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
-router.get("/:me", auth, async (req, res) => {
+router.get("/me", auth, async (req, res) => {
   const user = await User.findById(req.user._id).select("-password");
   res.send(user);
 });
